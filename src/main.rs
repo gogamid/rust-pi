e5fa153e70df3fb49242307b09970d13ed06a6b3
gogamid <gamidliimron@gmail.com>
2023-02-03 15:50:22 +0100

    feat: pi calculation  into par iter with rayon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate rayon;
 const N: usize = 1_000_000;
 const W: f64 = 1f64 / N as f64;
 
@@ -15,8 +16,10 @@ fn main() {
      println!("pi = {}", W * sum);*/
 
     //good way functional programming
+    use rayon::prelude::*;
     let sum: f64 = (0..N)
-        .into_iter()
+        // .into_iter() //stupid because it does one by one
+        .into_par_iter() //good way because it does parallel
         .map(|i| f(W * (i as f64 + 0.5)))
         .sum::<f64>();
     println!("pi = {}", W * sum);
